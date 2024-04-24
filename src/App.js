57ab51ce5d7dfa2f0e9338ba7ed201ae338ef04e
vineethsankre <lsvineethsankre@gmail.com>
2024-04-24 13:27:55 +0000

    Routing practice

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import NotFound from './components/NotFound'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Header from './components/Header'
+import './App.css'
+
+const App = () => (
+  <div className="app-container">
+    <div className="responsive-container">
+      <Header />
+      <div className="app-body">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </div>
+  </div>
+)
+
+export default App
---
+++ src/components/About/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+const About = () => (
+  <div className="about-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+      alt="about"
+      className="about-image"
+    />
+    <h1 className="about-heading">About</h1>
+    <p className="about-paragraph">
+      I love to create! I am a frontend developer
+    </p>
+  </div>
+)
+export default About
